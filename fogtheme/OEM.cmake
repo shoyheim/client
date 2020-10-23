7f54ceca95c62c247a23363670a6b5f3ac7cce8b
Sigvald.hoyheim <sigvald.hoyheim@fjordane.serit.no>
2020-10-23 07:57:07 +0200

    update theme file

--- fogtheme/OEM.cmake
+++ fogtheme/OEM.cmake
@@ -5,6 +5,8 @@ set( APPLICATION_DOMAIN     "fog.fjordane-it.no" )
 set( APPLICATION_VENDOR     "Serit Fjordane IT AS" )
 set( APPLICATION_UPDATE_URL "https://deploy.fjit.no/skylagring/client/" CACHE string "URL for updater" )
 set( APPLICATION_CLSID      "{2414F7C5-560F-4658-A176-2C8CC61B34CE}" )
+set( APPLICATION_ICON_NAME  "InternSky" )
+set( APPLICATION_VIRTUALFILE_SUFFIX "InternSky" CACHE STRING "Virtual file suffix (not including the .)")
 
 set( THEME_CLASS            "MyTheme" )
 set( APPLICATION_REV_DOMAIN "no.fjordane-it.fog.desktopclient" )
@@ -12,9 +14,12 @@ set( WIN_SETUP_BITMAP_PATH  "${OEM_THEME_DIR}/resources" )
 
 set( CPACK_PACKAGE_ICON  "${OEM_THEME_DIR}/resources/skylagring.ico" )
 
-# set( MAC_INSTALLER_BACKGROUND_FILE "${CMAKE_SOURCE_DIR}/admin/osx/installer-background.png" CACHE STRING "The MacOSX installer background image")
+set( MAC_INSTALLER_BACKGROUND_FILE "${CMAKE_SOURCE_DIR}/admin/osx/installer-background.png" CACHE STRING "The MacOSX installer background image")
 
 set( THEME_INCLUDE          "${OEM_THEME_DIR}/mytheme.h" )
 # set( APPLICATION_LICENSE    "${OEM_THEME_DIR}/license.txt )
 
-set( MIRALL_VERSION_SUFFIX "")
+option( WITH_CRASHREPORTER "Build crashreporter" OFF )
+set( CRASHREPORTER_SUBMIT_URL "https://crash-reports.owncloud.com/submit" CACHE STRING "URL for crash reporter" )
+
+
