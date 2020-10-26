32e2ad96d343347e90bea5c23c33bb034a0472fc
Sigvald.hoyheim <sigvald.hoyheim@fjordane.serit.no>
2020-10-26 11:22:29 +0100

    qt-string issues

--- fogtheme/mytheme.h
+++ fogtheme/mytheme.h
@@ -88,10 +88,7 @@ public:
     virtual QString about() const
     {
         QString devString;
-        devString = trUtf8("<p>Version %2. For more information visit <a href=\"%3\">%4</a></p>"
-                           "<p><small>By Serit Fjordane IT AS</small></p>"
-                           "<p>Copyright Serit Fjordane IT</p>"
-                           "<p>Licensed under the GNU General Public License (GPL) Version 2.0<br/></p>")
+        devString = tr("<p>Version %2. For more information visit <a href=\"%3\">%4</a></p> <p><small>By Serit Fjordane IT AS</small></p> <p>Copyright Serit Fjordane IT</p> <p>Licensed under the GNU General Public License (GPL) Version 2.0<br/></p>")
                         .arg(MIRALL_VERSION_STRING)
                         .arg("https://" MIRALL_STRINGIFY(APPLICATION_DOMAIN))
                         .arg(MIRALL_STRINGIFY(APPLICATION_DOMAIN));
